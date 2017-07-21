37f87187ef23af845c2e7b2093be8366d20a9b5d
James Nesbitt <james.nesbitt@claranet.uk>
2017-07-21 14:02:16 +0100

    Minor fixes

--- outputs.tf
+++ outputs.tf
@@ -3,20 +3,22 @@ output "replication_instance_arn" {
   value = "${aws_dms_replication_instance.repinstance.replication_instance_arn}"
 }
 
-/* output "replication_instance_private_ips" { */
-/*   value = "${aws_dms_replication_instance.replication_instance_private_ips}" */
-/* } */
+output "replication_instance_private_ips" {
+  value = "${aws_dms_replication_instance.replication_instance_private_ips}"
+}
 
-/* output "replication_instance_public_ips" { */
-/*   value = "${aws_dms_replication_instance.replication_instance_public_ips}" */
-/* } */
+output "replication_instance_public_ips" {
+  value = "${aws_dms_replication_instance.replication_instance_public_ips}"
+}
 
 /* // endpoint outputs */
 /* output "endpoint_arn" { */
 /*   value = "${aws_dms_replication_instance.replication_instance_public_ips}" */
 /* } */
 
+
 /* // subnet ouputs */
 /* output "subnet_vpc_id" { */
 /*   value = "${aws_dms_replication_subnet_group.subnet}" */
 /* } */
+
--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@
 variable "envname" {}
 variable "envtype" {}
 variable "service" {}
+
 variable "enabled" {
   default = true
 }
@@ -24,23 +25,30 @@ variable "source_endpoint_id" {}
 variable "source_endpoint_name" {}
 variable "source_engine_name" {}
 variable "source_server_name" {}
+
 variable "source_port" {
   default = "3306"
 }
+
 variable "source_username" {}
 variable "source_password" {}
+
 variable "source_database_name" {
   default = ""
 }
+
 variable "source_certificate_arn" {
   default = ""
 }
+
 variable "source_extra_connection_attributes" {
   default = ""
 }
+
 variable "source_kms_key_arn" {
   default = ""
 }
+
 variable "source_ssl_mode" {
   default = ""
 }
@@ -49,24 +57,31 @@ variable "source_ssl_mode" {
 variable "target_endpoint_id" {}
 variable "target_engine_name" {}
 variable "target_password" {}
+
 variable "target_port" {
   default = "3306"
 }
+
 variable "target_server_name" {}
 variable "target_username" {}
 variable "target_endpoint_name" {}
+
 variable "target_certificate_arn" {
   default = ""
 }
+
 variable "target_database_name" {
   default = ""
 }
+
 variable "target_extra_connection_attributes" {
   default = ""
 }
+
 variable "target_kms_key_arn" {
   default = ""
 }
+
 variable "target_ssl_mode" {
   default = ""
 }
@@ -74,6 +89,7 @@ variable "target_ssl_mode" {
 // Replication subnet variables
 variable "replication_subnet_group_description" {}
 variable "replication_subnet_group_id" {}
+
 variable "replication_subnet_subnet_ids" {
   type = "list"
 }
@@ -86,27 +102,35 @@ variable "replication_instance_name" {}
 variable "replication_instance_allocated_storage" {
   default = ""
 }
+
 variable "replication_instance_apply_immediately" {
   default = ""
 }
+
 variable "replication_instance_auto_minor_version_upgrade" {
   default = ""
 }
+
 variable "replication_instance_availability_zone" {
   default = ""
 }
+
 variable "replication_instance_engine_version" {
   default = ""
 }
+
 variable "replication_instance_kms_key_arn" {
   default = ""
 }
+
 variable "replication_instance_multi_az" {
   default = ""
 }
+
 variable "replication_instance_preferred_maintenance_window" {
   default = "sun:03:45-sun:05:45"
 }
+
 variable "replication_instance_publicly_accessible" {
   default = ""
 }
@@ -116,9 +140,11 @@ variable "replication_task_id" {}
 variable "replication_task_name" {}
 variable "replication_task_migration_type" {}
 variable "replication_task_table_mappings" {}
+
 variable "replication_task_settings" {
   default = ""
 }
+
 variable "replication_task_cdc_start_time" {
   default = ""
 }
