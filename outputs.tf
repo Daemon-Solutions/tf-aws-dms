26797eb8041a8ea7347b9a2805406968e9dc21a6
Lukas Wojnicki <lukas.wojnicki@claranet.uk>
2017-07-24 07:49:11 +0100

    commented out for debugging

--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,13 @@ output "replication_instance_arn" {
   value = "${aws_dms_replication_instance.repinstance.replication_instance_arn}"
 }
 
-output "replication_instance_private_ips" {
-  value = "${aws_dms_replication_instance.replication_instance_private_ips}"
-}
+/* output "replication_instance_private_ips" { */
+/*   value = "${aws_dms_replication_instance.replication_instance_private_ips}" */
+/* } */
 
-output "replication_instance_public_ips" {
-  value = "${aws_dms_replication_instance.replication_instance_public_ips}"
-}
+/* output "replication_instance_public_ips" { */
+/*   value = "${aws_dms_replication_instance.replication_instance_public_ips}" */
+/* } */
 
 /* // endpoint outputs */
 /* output "endpoint_arn" { */
