6bb6f1d56ffee41556ebd8f85b419f55e7fd79e5
Lukas Wojnicki <lukas.wojnicki@claranet.uk>
2019-09-05 09:06:42 +0100

    Adding source and destination arn to outputs

--- outputs.tf
+++ outputs.tf
@@ -16,6 +16,14 @@ output "endpoint_arn" {
   value = "${aws_dms_replication_instance.replication_instance_public_ips}"
 }
 
+output "source_endpoint_arn" {
+  value = "${aws_dms_endpoint.source_endpoint.endpoint_arn}"
+}
+
+output "target_endpoint_arn" {
+  value = "${aws_dms_endpoint.target_endpoint.endpoint_arn}"
+}
+
 // subnet ouputs
 output "subnet_vpc_id" {
   value = "${aws_dms_replication_subnet_group.subnet}"
